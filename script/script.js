b8b01358d22b074524645d6bd9241dbc6db1e9fb
raimasaniyat <raimasaniyat@gmail.com>
2026-02-24 06:44:10 +0600

    javascript coding started

--- script/script.js
+++ script/script.js
@@ -1,10 +1,19 @@
+let interviewList = [];
+let rejectedList =[];
+
 let total = document.getElementById('total');
 let interviewCount = document.getElementById('interviewCount');
 let rejectedCount = document.getElementById('rejectedCount');
 
 const allCardSection = document.getElementById('allCards');
 
+// interviewList.push({name: 'hello'},{name: 'hello'});
+const mainContainer = document.getElementById('button-parent');
+console.log(mainContainer);
+
 function calcCount(){
-    total.innerText = allCardSection.children.length
+    total.innerText = allCardSection.children.length;
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
 }
 calcCount()
